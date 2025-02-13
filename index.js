2e170e244597c9ded85cafd6c583e829e90282ec
Student Newton School <aradhya.tiwari2024@nst.rishihood.edu.in>
2025-02-13 11:48:57 +0530

    Some More Progress

--- index.js
+++ index.js
@@ -11,3 +11,44 @@ const winConditions = [
     [0,4,8],
     [2,4,6]
 ]
+let options = ["","","","","","","","",""]
+let currentPlayer = 'X';
+let running = false;
+
+initializeGame()
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click",restartGame);
+    statusText.textContent = `${currentPlayer}'s Turn `;
+    running = true;
+}
+
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex")
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+    updateCell(this, cellIndex);
+    // changePlayer();
+    checkWinner();
+}
+
+function updateCell(cell,index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+
+}
+function  changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s Turn`
+
+}
+function checkWinner(){
+    
+
+}
+function restartGame(){
+
+}
